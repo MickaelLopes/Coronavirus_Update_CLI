b99ec9d68f12529b4837ab4af7c43b38878c839a
Mickael <lopesmickael8@gmail.com>
2020-06-22 18:39:28 +1000

    Update Test scenario with connectivity and data extraction tests

--- tests/context.py
+++ tests/context.py
@@ -1,8 +1,14 @@
 """
-Insert the project library as local package direcotry in order to be able to call the function for testing 
+Insert the project library as local package direcotry in order to be able to call the function for testing and provide path for testing ressources
 """
 
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+test_ressources_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'tests_resources'))
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+sys.path.insert(0,project_path)
+
+TEST_WORLD_URL = test_ressources_path + '/Worldometers_MainPage.html'
--- tests/test_datarequest.py
+++ tests/test_datarequest.py
@@ -1,14 +1,71 @@
 import sys
 import unittest
-
 import context
+import requests
 from coronavirus_cli.datarequest.utils import _normalize_country_name
+from coronavirus_cli.datarequest.global_data import GlobalData
+from coronavirus_cli.datarequest import urls
+from bs4 import BeautifulSoup
 
-# print(sys.path)
-class unittest_datarequest(unittest.TestCase): 
-    
+class unittest_utils_function(unittest.TestCase): 
+    """
+    Basic Testing for the utils function in the datarequest module
+    """
+
     def test_normalize_string(self):
         self.assertEqual(_normalize_country_name('USA'), 'USA')        
         self.assertEqual(_normalize_country_name('S. Korea'), 'S_Korea')
         self.assertEqual(_normalize_country_name('portugal'), 'Portugal')
         self.assertEqual(_normalize_country_name('new zealand'), 'New_Zealand')
+
+class unittest_connectivity(unittest.TestCase):
+    """
+    Test the connectivity to request the url and parsed them with beautifoul soup
+    """
+
+    def setUp(self): 
+        self.r_test = requests.get(urls.World)
+    
+    def test_connectivity(self): 
+         self.assertEqual(self.r_test.status_code, 200)
+
+    def test_parsing(self): 
+        try: 
+            test_soup = BeautifulSoup(self.r_test.text, 'html.parser')
+        except : 
+            test_parsing_successful = False
+        else : 
+            test_parsing_successful = True
+        self.assertEqual(test_parsing_successful,True)
+class unittest_global_data(unittest.TestCase): 
+    """
+    Testing of the world_data module meant to extract data from world webpage 
+    """
+
+    def setUp(self): 
+        # self.soup_world_test = BeautifulSoup(open(context.TEST_WORLD_URL), 'html.parser')
+        self.test_global_world = GlobalData(test_url=context.TEST_WORLD_URL)
+    def test_world_data(self): 
+        self.assertEqual(self.test_global_world.get_number_cases(), 9051535)
+        self.assertEqual(self.test_global_world.get_number_active_case(), 3738783)
+        self.assertEqual(self.test_global_world.get_number_revover(), 4841948)
+        self.assertEqual(self.test_global_world.get_number_death(), 470804)
+        
+    def test_country_data_USA(self): 
+        self.assertEqual(self.test_global_world.get_number_cases(country='USA'), 2356657)
+        self.assertEqual(self.test_global_world.get_number_active_case(country='USA'), 1254055)
+        self.assertEqual(self.test_global_world.get_number_revover(country='USA'), 980355)
+        self.assertEqual(self.test_global_world.get_number_death(country='USA'), 122247)
+    
+    def test_country_data_South_Korea(self): 
+        self.assertEqual(self.test_global_world.get_number_cases(country='S_Korea'), 12438)
+        self.assertEqual(self.test_global_world.get_number_active_case(country='S_Korea'), 1277)
+        self.assertEqual(self.test_global_world.get_number_revover(country='S_Korea'), 10881)
+        self.assertEqual(self.test_global_world.get_number_death(country='S_Korea'), 280)
+    
+    def test_country_data_Trinidad_And_Tobago(self): 
+        self.assertEqual(self.test_global_world.get_number_cases(country='Trinidad_And_Tobago'), 123)
+        self.assertEqual(self.test_global_world.get_number_active_case(country='Trinidad_And_Tobago'), 6)
+        self.assertEqual(self.test_global_world.get_number_revover(country='Trinidad_And_Tobago'), 109)
+        self.assertEqual(self.test_global_world.get_number_death(country='Trinidad_And_Tobago'), 8)
+    
