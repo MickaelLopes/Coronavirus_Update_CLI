9e16c294c55f8b6c4c76fc1c038961502076f206
Mickael <lopesmickael8@gmail.com>
2020-06-30 13:21:41 +1000

    change main to run the CLI interface

--- coronavirus_cli/main.py
+++ coronavirus_cli/main.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python
+from interface.command import StartCommand
+from cleo import Application
 
-# from interface.command import StartCommand
-# from cleo import Application
-
-from datarequest.global_data import GlobalData
-from datarequest.utils import _find_country_row
-# app = Application()
-# app.add(StartCommand())
+# from datarequest.global_data import GlobalData
+# from datarequest.utils import _find_country_row
+app = Application()
+app.add(StartCommand())
 
 if __name__ == '__main__': 
-    # app.run()
-    g = GlobalData()
-    print(_find_country_row(g.soup, 'USA'))
+    app.run()
+    # g = GlobalData()
+    # print(_find_country_row(g.soup, 'USA'))
