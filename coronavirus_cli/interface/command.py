d9597d3dd419c1a8da6cae100080fbbae918a07c
Mickael <lopesmickael8@gmail.com>
2020-07-17 16:27:40 +1000

    Update the promt of data and add tabulate function

--- coronavirus_cli/interface/command.py
+++ coronavirus_cli/interface/command.py
@@ -1,43 +1,51 @@
 # from .context import *
 from cleo import Command
 from datarequest.global_data import GlobalData
-
+from tabulate import tabulate
+import sys
 
 with open('datarequest/countries.txt', 'r') as f:
     list_countries = f.readlines()
     list_countries = list(map(lambda x : x.strip('\n'), list_countries))
 
-class StartCommand(Command):
+class RequestCommand(Command):
     """
-    Start Command 
+    Request Command 
 
-    start
-        { --a|all : all data (case, active, recover, death)}
+    request
         { --c|case : number of cases}
         { --t|active : number of active cases}
         { --r|recover : number of recover cases}
         { --d|death : number of deaths}
         { --p|country : data from specific country}
+        { --l|list : display list of countries }
     """ 
-    def handle(self):
-        
-        self.line("Greetings and Welcome")
+    def handle(self):    
         corona_data = GlobalData()
-        country = 'World'
+        data = []
+        show_all_values = False
+        if self.option('list'):
+            self.line('\n'.join(list_countries))
+            sys.exit()
         if self.option('country'): 
-            question =self.create_question('which country ?', default = 'USA')
+            question =self.create_question('Target country ?', default = 'USA')
             question.set_autocomplete_values(list_countries)
             choice = self.ask(question)
             # self.line("Choose among the list of given countries : \n")
             # self.line('\t'.join(list_countries))
             country = choice
-
-        if self.option('case') or self.option('all') : 
-            print(f'Number of cases : \t{corona_data.get_number_cases(country = country)}')
-        if self.option('active') or self.option('all'):
-            print(f'Number of actives cases : \t {corona_data.get_number_active_case(country = country)}')
-        if self.option('recover') or self.option('all'): 
-            print(f'Number of recover cases : \t{corona_data.get_number_recover(country = country)}')
-        if self.option('death') or self.option('all'):
-            print(f'Number of death : \t {corona_data.get_number_death(country = country)}')
-            
+            self.line(f'\nData for country {country}\n')
+        if not(self.option('country')):
+            country = 'World'
+            self.line('\nWorld data\n')
+        if not(self.option('case')) and not(self.option('active')) and not(self.option('recover')) and not(self.option('death')):
+            show_all_values = True
+        if self.option('case') or show_all_values : 
+            data.append(['Total Cases', corona_data.get_number_cases(country = country)])
+        if self.option('active') or show_all_values:
+            data.append(['Active Cases', corona_data.get_number_active_case(country = country)])
+        if self.option('recover') or show_all_values:
+            data.append(['Recovered Cases', corona_data.get_number_recover(country = country)]) 
+        if self.option('death') or show_all_values:
+            data.append(['Deaths', corona_data.get_number_death(country = country)])
+        print(tabulate(data))
--- coronavirus_cli/main.py
+++ coronavirus_cli/main.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
-from interface.command import StartCommand
+# !/usr/bin/env python3.7
+from interface.command import RequestCommand
 from cleo import Application
 
 # from datarequest.global_data import GlobalData
 # from datarequest.utils import _find_country_row
 app = Application()
-app.add(StartCommand())
+app.add(RequestCommand())
 
 if __name__ == '__main__': 
     app.run()
